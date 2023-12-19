47a6db73283711345e5e4a3401b62dc9cd92358d
midhey <midheyss@gmail.com>
2023-12-19 22:46:10 +0300

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'http://localhost:8080/',
 });
+
+api.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export default api;
