90359bc17acd5b0bc22af68c423c1ad633012405
midhey <midheyss@gmail.com>
2023-12-19 22:51:03 +0300

    fuck it

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: 'http://localhost:8080/',
 });
 
-api.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+api.defaults.headers.common['Content-Type'] ='application/json;charset=utf-8';
+api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 export default api;
